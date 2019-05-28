017608437be1681bdbce14b39e455271771eb00d
premiumsnap <50746390+premiumsnap@users.noreply.github.com>
2019-05-28 20:10:24 +0200

    Add files via upload

---
+++ sw-0bf6b.js
@@ -0,0 +1,6 @@
+self.options = {
+                      "domain": "jeejujou.net",
+                      "zoneId": 2646539
+                    }
+                    self.lary = ""
+                    importScripts('https://jeejujou.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
